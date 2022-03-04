4a8fcff3accda3212f4f6d5ec9b9babd9c496c65
yansy10 <649239538@qq.com>
2022-03-04 14:30:32 +0800

    init

---
+++ hello.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"example/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
+	//"gitlab.sanywind.net/sany/windsmartscadaplus/octopus/common/consts"
+)
+
+
+
+func main(){
+	fmt.Println("hello world")
+	fmt.Println(morestrings.ReverseRunes("Hello,Go!"))
+	fmt.Println(cmp.Diff("Hello World","Hello Go"))
+	//fmt.Println(consts.DateTimeTemplate)
+
+	a := []struct{in,out Count}{{"1","2"},{"3","4"}}
+	fmt.Println(a)
+	for index,v := range a {
+		fmt.Println(index,v.in,v.out,v.in.ServeHTTP(string(v.out)))
+	}
+
+}
+
+type Handler interface {
+    ServeHTTP(x string)
+}
+
+type Count string;
+
+func (ctr Count) ServeHTTP(x string) string{
+	return string(ctr) + x
+}
+
